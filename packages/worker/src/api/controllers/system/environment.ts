ab28d18dd6de66bc859f14afdf96e75b1a852642
IchthysMaranathaCopy <servantofchristjesus@yahoo.com>
2024-03-13 17:50:06 +0530

    Add files via upload

--- packages/worker/src/api/controllers/system/environment.ts
+++ packages/worker/src/api/controllers/system/environment.ts
@@ -1,14 +1,41 @@
-import { BBContext } from "@budibase/types"
+import { Ctx } from "@budibase/types"
 import env from "../../../environment"
+import { env as coreEnv } from "@budibase/backend-core"
+import nodeFetch from "node-fetch"
 
-export const fetch = async (ctx: BBContext) => {
+let sqsAvailable: boolean
+async function isSqsAvailable() {
+  if (sqsAvailable !== undefined) {
+    return sqsAvailable
+  }
+
+  try {
+    await nodeFetch(coreEnv.COUCH_DB_SQL_URL, {
+      timeout: 1000,
+    })
+    sqsAvailable = true
+    return true
+  } catch (e) {
+    sqsAvailable = false
+    return false
+  }
+}
+
+export const fetch = async (ctx: Ctx) => {
   ctx.body = {
     multiTenancy: !!env.MULTI_TENANCY,
+    offlineMode: !!coreEnv.OFFLINE_MODE,
     cloud: !env.SELF_HOSTED,
     accountPortalUrl: env.ACCOUNT_PORTAL_URL,
     disableAccountPortal: env.DISABLE_ACCOUNT_PORTAL,
     baseUrl: env.PLATFORM_URL,
-    // in test need to pretend its in production for the UI (Cypress)
     isDev: env.isDev() && !env.isTest(),
+    directoidc: env.DIRECT_OIDC,
+  }
+
+  if (env.SELF_HOSTED) {
+    ctx.body.infrastructure = {
+      sqs: await isSqsAvailable(),
+    }
   }
 }
